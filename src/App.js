d21948aa511e24554d7b448d1977ca7b82d35728
Musaiyib Yakubu Usman <myusoro@gmail.com>
2021-10-16 05:45:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+
+import PostList from './components/posts/PostList'
+import PostCreate from './components/posts/PostCreate'
+import PostEdit from './components/posts/PostEdit'
+
+import UserList from './components/users/UserList'
+import UserCreate from './components/users/UserCreate'
+import UserEdit from './components/users/UserEdit'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin dataProvider={restProvider('http://localhost:3000')} >
+        <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
+        <Resource name='users' list={UserList} create={UserCreate} edit={UserEdit} />
+      </Admin>
     </div>
   );
 }
---
+++ src/components/users/UserList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { List, Datagrid, TextField, EmailField, EditButton, DeleteButton } from 'react-admin';
+
+const UserList = (props) => {
+  return<List {...props}>
+    <Datagrid>
+      <TextField source='id' />
+      <TextField source='title' />
+      <EmailField source='email' />
+      <EditButton basePath='/users' />
+      <DeleteButton basePath='/users' />
+    </Datagrid>
+  </List>
+}
+
+export default UserList
